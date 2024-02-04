0e904ba0dc0ea57430f333b21ef37191a763cbf6
Mehdi Tarhoult <mehdi.trh@outlook.fr>
2024-02-04 22:15:13 +0100

    Handled messages for meeting creation process

--- meeting_planner/meetings/views.py
+++ meeting_planner/meetings/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-
+from django.contrib import messages
 from .models import Meeting, Room
 from .forms import MeetingForm
 
@@ -8,7 +8,8 @@ def detail (request, id):
     return render(request, "meetings/detail.html", {"meeting": meeting})
 
 def rooms_list (request):
-    return render (request, "meetings/rooms_list.html",
+    return render (request,
+                   "meetings/rooms_list.html",
                    {"rooms": Room.objects.all()})
 
 def new (request):
@@ -16,7 +17,16 @@ def new (request):
         form = MeetingForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.success(
+                request,
+                f"Successfully saved {form.cleaned_data['title']}"
+            )
             return redirect("welcome")
+        else:
+            messages.error(
+                request,
+                'Failed to create a new meeting... Please try again.'
+            )
     else:
         form = MeetingForm()
     return render (request, "meetings/new.html", {"form": form})
